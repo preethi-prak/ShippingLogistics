2b4e0e759e61206ffe7df5efbdc92f50ae38d4b1
Preethi Prakash <Preethi93@icloud.com>
2023-12-10 23:40:34 +0000

    #3 - Modal Show javascript file

--- STHT/wwwroot/js/site.js
+++ STHT/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    // Open Bid modal on button click
+    $('#bidModalBtn').click(function () {
+        $('#bidModal').modal('show');
+    });
+
+    // Open Shipping modal on link click
+    $('#shippingModalLink').click(function () {
+        $('#shippingModal').modal('show');
+    });
+});
