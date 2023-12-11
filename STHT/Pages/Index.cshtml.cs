a19e6418386b4916f484287d3cbc6bf8998cf46f
Preethi Prakash <Preethi93@icloud.com>
2023-12-11 19:30:58 +0000

    #6 Stable Commit - Index.cshtml.cs

--- STHT/Pages/Index.cshtml.cs
+++ STHT/Pages/Index.cshtml.cs
@@ -5,41 +5,49 @@ namespace STHT.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    private readonly IHttpClientFactory _httpClientFactory;
+    //private readonly IHttpClientFactory _httpClientFactory;
     //private readonly UserShippingDbContext _dbContext;
 
     //include parameter UserShippingDbContext dbContext 
+    //IHttpClientFactory httpClientFactory
     //Dependency injection
-    public IndexModel(IHttpClientFactory httpClientFactory, ILogger<IndexModel> logger)
+    public IndexModel( ILogger<IndexModel> logger)
     {
-        _httpClientFactory = httpClientFactory;
+       //_httpClientFactory = httpClientFactory;
         //_dbContext = dbContext;
         _logger = logger;
     }
     public string UserLocaleCountry { get; set; }
     public string ApiResponse { get; set; }
 
-    public async Task OnGet()
+    //async Task
+    public void OnGet()
     {
-        await GetUserLocale();
-        await MakeApiCall();
-        ProcessShippingData();
+        //await GetUserLocale();
+        //await MakeApiCall();
+        //ProcessShippingData();
     }
 
-    
+   // API Call to make user locale
     private async Task GetUserLocale()
     {
         throw new NotImplementedException();
     }
+    //Api call to make external API call
     
     private async Task MakeApiCall()
     {
         throw new NotImplementedException();
     }
     
+    //Method to save Shipping Data in the database
     private void ProcessShippingData()
     {
         throw new NotImplementedException();
     }
+
+
+    
+    
 }
 
