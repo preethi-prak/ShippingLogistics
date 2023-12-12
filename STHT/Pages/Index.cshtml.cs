0067d16caad8e2c41b4dbc55e07c792be00de38b
Preethi Prakash <Preethi93@icloud.com>
2023-12-12 17:02:16 +0000

    #9 Shipping Data API call - sucess

--- STHT/Pages/Index.cshtml.cs
+++ STHT/Pages/Index.cshtml.cs
@@ -1,74 +1,89 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json; //  Newtonsoft.Json for JSON 
 
-namespace STHT.Pages;
 
-public class IndexModel : PageModel
+namespace STHT.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-    private readonly IHttpClientFactory _httpClientFactory;
-    //private readonly UserShippingDbContext _dbContext;
-
-    //include parameter UserShippingDbContext dbContext 
-    //IHttpClientFactory httpClientFactory
-    //Dependency injection
-    public IndexModel(IHttpClientFactory httpClientFactory, ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-       //_httpClientFactory = httpClientFactory;
-        //_dbContext = dbContext;
-        _logger = logger;
-        _httpClientFactory = httpClientFactory;
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-    }
-
-    public string? UserLocaleCountry { get; set; } 
-    public string? ShippingApiResponse { get; set; } 
-
-    //async Task
-    public void  OnGet()
-    {
-        //await GetUserLocale();
-        //await ShippingApiCall();
-        UserLocaleCountry = "FR";
-
-        //ProcessShippingData();
-    }
-
-   // API Call to make user locale
-    //private async Task GetUserLocale()
-    //{
-        //UserLocaleCountry = "FR";
-    //}
-    //Api call to make external API call
-    
-    private async Task ShippingApiCall()
-    {
-        var client = _httpClientFactory.CreateClient("ShippingDataClient");
-        //var bearerToken = "20231207160500";
-        //client.DefaultRequestHeaders.Add("Authorization", $"Bearer {bearerToken}");
-        //var response = await client.GetAsync("https://new-api.staging.spectinga.com/STHT/shipping");
-        var response = await client.GetAsync("https://cat-fact.herokuapp.com/facts/");
-
-        if (response.IsSuccessStatusCode)
+        public IndexModel(IHttpClientFactory httpClientFactory, ILogger<IndexModel> logger)
         {
-            ShippingApiResponse = await response.Content.ReadAsStringAsync();
-            _logger.LogInformation(response.ReasonPhrase);
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
         }
-        else
+
+        public string? UserLocaleCountry { get; set; } 
+        public string? ShippingApiResponse { get; set; }
+        public Dictionary<string, decimal>? ShippingData { get; private set; }
+
+        public async Task OnGetAsync()
         {
-            ShippingApiResponse = $"Error: {response.StatusCode}";
-            _logger.LogCritical(response.ReasonPhrase);
+            // await GetUserLocale();
+            UserLocaleCountry = "FR"; 
+            ShippingData = await ShippingApiCallAsync();
+            // ProcessShippingData();
+            
+            if (ShippingData is not null)
+            {
+                foreach(var item in ShippingData)
+                {
+                    Console.Write(item.Key);
+                    Console.Write(item.Value);
+                    Console.WriteLine("/n");
+                
+                }
+                
+            }
+            
         }
-        
-    }
-    
-    //Method to save Shipping Data in the database
-    private void ProcessShippingData()
-    {
-        throw new NotImplementedException();
-    }
 
+        private async Task<Dictionary<string, decimal>?> ShippingApiCallAsync()
+        {
+            var httpClient = _httpClientFactory.CreateClient("ShippingDataClient");
+            var bearerToken = "20231207160500";
 
-    
-    
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {bearerToken}");
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "MyAspNetCoreApp/6.0");
+
+            try
+            {
+                //include URL and bearer token in appsetting.json - !IMPORTANT
+                var response = await httpClient.GetAsync("https://new-api.staging.spectinga.com/STHT/shipping");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var shippingResponse = await response.Content.ReadAsStringAsync();
+                    var shippingData = JsonConvert.DeserializeObject<Dictionary<string, decimal>>(shippingResponse);
+                    _logger.LogInformation(" API call Success");
+                    return shippingData;
+                }
+                else
+                {
+                    var error = $"Error: {response.StatusCode}";
+                    _logger.LogError($"API call failed: {error}");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Exception : {ex.Message}");
+                return null;
+            }
+        }
+
+        /*
+        private async Task GetUserLocale()
+        {
+            // Implementation here.
+        }
+
+        private void ProcessShippingData()
+        {
+            // Implementation here.
+        }
+        */
+    }
 }
-
