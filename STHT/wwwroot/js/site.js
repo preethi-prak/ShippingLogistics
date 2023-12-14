3a5a39c5ef7bb4d3ff544927a084f7628d7012bf
Preethi Prakash <Preethi93@icloud.com>
2023-12-14 22:56:24 +0000

    #17 Jquery for the Radio button to update the total price

--- STHT/wwwroot/js/site.js
+++ STHT/wwwroot/js/site.js
@@ -1,8 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-$(document).ready(function () {
+﻿// Write your JavaScript code.
+$(document).ready(function() {
     // Open Bid modal on button click
     $('#bidModalBtn').click(function () {
         $('#bidModal').modal('show');
@@ -24,8 +21,6 @@ $(document).ready(function () {
             $('#shippingModal').modal('hide');
         }
     });
-
-    
-        
-  
 });
+
+
